fddc1ea4448ed8a41e947c039e006439743a612a
abdurrahman720 <personal.abdurrahman@gmail.com>
2022-10-04 11:57:48 +0600

    completed cart section

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,12 +2,28 @@ import React from 'react';
 import './Cart.css'
 
 
-const Cart = (props) => {
-    const {cart} = props;
+const Cart = ({cart}) => {
+    console.log(cart)
+
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total += product.price;
+        shipping += product.shipping;
+    }
+
+    const tax = parseFloat((total * 0.1).toFixed(2));  //use parseFloat since toFixed returns a string instead of number.
+    const grandTotal = total + shipping + tax;
+    
+
     return (
-        <div>
-            <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length}</p>
+        <div className="cart">
+                <h4>Order Summary</h4>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price: ${total} </p>
+            <p>Total Shipping: ${shipping} </p>
+            <p>TAX: ${tax} </p>
+            <h5>Grand Total: ${grandTotal} </h5>
         </div>
     );
 };
