ac3c8004ed6bf55aae0f4cbdfaf426871db08e8e
abdurrahman720 <personal.abdurrahman@gmail.com>
2022-10-04 11:38:14 +0600

    added a new cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Cart.css'
+
+
+const Cart = (props) => {
+    const {cart} = props;
+    return (
+        <div>
+            <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
+        </div>
+    );
+};
+
+export default Cart;
